a0b7fe5af1d765466c5122e43aa8dfb41b536eb3
Michele <michelemascia1996@gmail.com>
2021-08-28 17:51:22 +0200

    Initial commit

---
+++ scripts.js
@@ -0,0 +1,16 @@
+let pokemonList = [
+  {
+    name: 'Charmander',
+    height: 0.6,
+    types: 'fire'
+  },
+  {
+    name: 'Bulbasaur',
+    height: 0.7,
+    types: ['grass','poison']
+  },
+  {
+    name: 'Squirtle',
+    height: 0.5,
+    types: 'water'}
+];
