537f18706e94c4b481931bbb51bc4107e7f95e6b
Michele <michelemascia1996@gmail.com>
2021-08-31 13:02:25 +0200

    Update scripts.js
    implemented shiny button

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,4 @@
+const shinyButton = document.querySelector('#shiny-button');
 let pokemonRepository = (function () {
   let pokemonList = [];
   let apiUrl = 'https://pokeapi.co/api/v2/pokemon/?limit=386';
@@ -21,6 +22,7 @@ let pokemonRepository = (function () {
     let pokemonGroup = document.querySelector('.list-group');
     let listpokemon = document.createElement('li');
     let button = document.createElement('button');
+
     listpokemon.classList.add('lista');
     listpokemon.classList.add('group-list-item');
     button.innerText = pokemon.name.charAt(0).toUpperCase() + pokemon.name.slice(1);
@@ -35,6 +37,7 @@ let pokemonRepository = (function () {
     button.addEventListener('click', function () {
       showDetails(pokemon);
     })
+
   }
   function showModal(item) {
     let modalContainer = document.querySelector('#modal-container');
@@ -93,12 +96,19 @@ let pokemonRepository = (function () {
     }).then(function (details) {
 
       item.imageUrl = details.sprites.front_default;
+      item.imageUrlShiny = details.sprites.front_shiny;
       item.height = details.height;
       item.types = details.types;
       item.weight = details.weight;
       item.abilities = details.abilities;
 
+
+
       showModal(item);
+      shinyButton.addEventListener('click',function(){
+
+        showShiny(item);
+      })
 
     }).catch(function (e) {
       console.error(e);
@@ -109,11 +119,22 @@ let pokemonRepository = (function () {
     const pkName = document.querySelector('.modal-title');
     const body = document.querySelector('.modal-body');
     const pkImage = document.createElement('img');
+
     pkName.innerText = pokemon.name.charAt(0).toUpperCase() + pokemon.name.slice(1);
     body.innerHTML = '<div> Height: ' +  pokemon.height + '</div> <div> Weight: ' +  pokemon.weight + '</div>'
     pkImage.src = pokemon.imageUrl;
     body.appendChild(pkImage);
   }
+
+  function showShiny(pokemon){
+    const shinyButton = document.querySelector('#shiny-button');
+    const shinyImg = document.createElement('img');
+    shinyImg.src = pokemon.imageUrlShiny;
+    const body = document.querySelector('.modal-body');
+    body.appendChild(shinyImg);
+  }
+
+
 
   return {
     add: add,
@@ -124,6 +145,7 @@ let pokemonRepository = (function () {
     loadDetails: loadDetails,
     showModal: showModal,
     modalInfo: modalInfo,
+    showShiny: showShiny
   };
 })();
 
